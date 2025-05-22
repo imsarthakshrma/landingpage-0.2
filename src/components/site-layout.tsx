dc460a6eb7eaba3d8f2da53db324ed0a2ae8a466
imsarthakshrma <imsarthakshrma@gmail.com>
2025-05-22 18:59:18 +0530

    feat: create site layout component with animated header and background image

--- src/components/site-layout.tsx
+++ src/components/site-layout.tsx
@@ -37,7 +37,12 @@ export  default function SiteLayout({ children }: { children: React.ReactNode })
                 <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 w-full">
                 {/* Header */}
                     <div className="flex justify-center py-4">
+                        <header
+                        ref={headerRef}
+                        className="flex justify-between items-center py-1 px-4 w-10/12 max-w-4xl rounded-lg bg-black/30 backdrop-blur-sm border-[2.2px] border-gray-700"
+                        >
                         {/* TODO: content implementation here */}
+                        </header>
                     </div>
                 </div>
             </div>
